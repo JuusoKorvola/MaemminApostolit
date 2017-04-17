b9cbfb8907df9b541443a663e19b4c4aa0d1734a
JuusoKorvola <juuso.korvola@gmail.com>
2017-04-18 00:03:44 +0300

    Lisätty javadoc, herjaus rivillä 20, ArrayOutOfBoundsException

--- src/SalasananVahvuus.java
+++ src/SalasananVahvuus.java
@@ -6,9 +6,17 @@ import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 
-
+/**
+ * Pääluokka SalasananVahvuus joka sisältää ohjelman funktioineen
+ * @author juuso_000
+ *
+ */
 public class SalasananVahvuus {   
 	private static final Scanner lukija = new Scanner(System.in);    
+	/**
+	 * Main funktioniin sisälletty ajastin funktio joka lopettaa ohjelman 30 sekunnin kuluessa. Tämä voidaan myös ajaa cmd linestä parametreillä tai ilman. Ilman parametria salasanansyöttö terminoituu 30 sekunnin kuluessa.
+	 * @param args String[] on siis taulukko tyyppiä, jossa annetaan indeksille arvoksi 30 sekuntia mikäli cmd linestä ei anneta jotakin toista parametria
+	 */
 	public static void main(String[] args) {  
 		int ajastin = 30;
 		if(args[0] != null){
@@ -24,6 +32,9 @@ public class SalasananVahvuus {
 		};
 		System.out.println("Sinulla on " + ajastin + " sekuntia aikaa antaa salasana");
 		timer.schedule(task, ajastin * 1000);
+		/**
+		 * Try catch joka ottaa kiinni filenotfoundexceptionin mikäli tiedostoa ei löydy tai se on väärässä paikassa tai se on esimerkiksi hakemisto.
+		 */
 		try {
 			String salasana = lueSalasana();  
 			while (salasananMuuttujat(salasana) == false){  
@@ -35,6 +46,11 @@ public class SalasananVahvuus {
 			System.out.println("Tiedostoa ei löytynyt.");
 		}
 	}  
+	/**
+	 * Tulostaa salasanan tiedostosta joka tallennettiin funktiossa lueSalasana, käytetään string -tyyppistä parametria "salasana" jotta annettu salasana saadaan käytettäväksi.
+	 * @param String salasana saadaan funktiosta lueSalasana
+	 * @throws FileNotFoundException mikäli tiedosto.txt ei ole olemassa.
+	 */
 	private static void tulostaSalasana(String salasana) throws FileNotFoundException {
 		final Scanner tLukija = new Scanner(new File("salasana.txt"));
 		System.out.println("Salasanasi on: " +tLukija.nextLine());
@@ -43,6 +59,11 @@ public class SalasananVahvuus {
 
 
 	}  
+	/**
+	 * Mukana taas parametri salasana. Lisätään listaan erilaisia merkkijonoja alkioihin joita käytetään salasananvahvuutta mittaavissa funktioissa.
+	 * @param salasana string joka saadaan lueSalasana funktiosta
+	 * @return False mikäli kaikki kriteerit eivät täyty
+	 */
 	public static boolean salasananMuuttujat(String salasana ) {
 		ArrayList<String> sanat = new ArrayList<String>();
 		sanat.add ("Salasana on alle 10 merkkiä pitkä");
@@ -55,7 +76,6 @@ public class SalasananVahvuus {
 		sanat.add ("Salasanasta puuttuu sana KAKKA");
 		sanat.add ("Salasanassa on kiellettyjä merkkejä ';','©','û'");
 
-		//System.out.println("Anna salasana, sen tulee sisältää ainakin: " + sanat + " Salasana ei saa siältää välilyöntejä, huutomerkkejä eikä se saa olla yli 50 merkkiä pitkä.");
 
 
 		boolean testi = true;
@@ -64,12 +84,19 @@ public class SalasananVahvuus {
 		if(!salasananTarkistaMerkit(salasana, sanat)){testi = false;}
 		if(!salasananHUUTO(salasana, sanat)){testi = false;}
 		if(!salasananAekkoset(salasana, sanat)){testi = false;}
-		if(!salasananKAKKA(salasana, sanat)){testi = false;}
+		if(!salasananData(salasana, sanat)){testi = false;}
 		if(!salasananVaelilyonti(salasana, sanat)){testi = false;}
 		if(!salasananIsotKirjaimet(salasana, sanat)){testi = false;}
 		if(!salasananYli50merkkia(salasana, sanat)){testi = false;}
 		return testi;  
 	}  
+	
+	/**
+	 * Mikäli salasana on yli 50 merkkiä pitkä, palauttaa funktio arvon false
+	 * @param salasana String tyyppinen parametri saadaan lueSalasana funktiosta
+	 * @param sanat Listasta löytyvät printlinet jotka kertovat mitä funktiossa vaaditaan käyttäjältä
+	 * @return true jos salasana läpäisee kriteerin
+	 */
 	private static boolean salasananYli50merkkia(String salasana, ArrayList<String> sanat) {
 		if (salasana.length() > 50){  
 			System.out.println(sanat.get(6));  
@@ -77,6 +104,12 @@ public class SalasananVahvuus {
 		}
 		return true;
 	}
+	/**
+	 * mikäli salasanassa ei ole isoja kirjaimia, palauttaa funktio arvon false
+	 * @param salasana String tyyppinen parametri saadaan lueSalasana funktiosta
+	 * @param sanat Listasta löytyvät printlinet jotka kertovat mitä funktiossa vaaditaan käyttäjältä
+	 * @return true jos salasana läpäisee kriteerin
+	 */
 	private static boolean salasananIsotKirjaimet(String salasana, ArrayList<String> sanat) {
 		if(salasana.equals(salasana.toLowerCase())){
 			System.out.println(sanat.get(1));
@@ -85,6 +118,12 @@ public class SalasananVahvuus {
 
 		return true;
 	}
+	/**
+	 * Mikäli salasana sisältää välilyönnin, palauttaa funktio arvon false
+	 * @param salasana String tyyppinen parametri saadaan lueSalasana funktiosta
+	 * @param sanat Listasta löytyvät printlinet jotka kertovat mitä funktiossa vaaditaan käyttäjältä
+	 * @return true jos salasana läpäisee kriteerin
+	 */
 	private static boolean salasananVaelilyonti(String salasana, ArrayList<String> sanat) {
 		if (salasana.contains(" ")){ 
 			System.out.println(sanat.get(4)); 
@@ -92,13 +131,25 @@ public class SalasananVahvuus {
 		} 
 		return true;
 	}
-	private static boolean salasananKAKKA(String salasana, ArrayList<String> sanat) {
-		if (!salasana.contains("KAKKA")){ 
+	/**
+	 * Mikäli salasana ei sisällä sanaa "data" palauttaa funktio arvon false
+	 * @param salasana String tyyppinen parametri saadaan lueSalasana funktiosta
+	 * @param sanat Listasta löytyvät printlinet jotka kertovat mitä funktiossa vaaditaan käyttäjältä
+	 * @return true jos salasana läpäisee kriteerin
+	 */
+	private static boolean salasananData(String salasana, ArrayList<String> sanat) {
+		if (!salasana.contains("data")){ 
 			System.out.println(sanat.get(7)); 
 			return false;
 		}  
 		return true;
 	}
+	/**
+	 * Mikäli salasanasta ei löydy ääkkösiä palauttaa funktio arvon false
+	 * @param salasana String tyyppinen parametri saadaan lueSalasana funktiosta
+	 * @param sanat Listasta löytyvät printlinet jotka kertovat mitä funktiossa vaaditaan käyttäjältä
+	 * @return true jos salasana läpäisee kriteerin
+	 */
 	private static boolean salasananAekkoset(String salasana, ArrayList<String> sanat) {
 		if(!salasana.matches(".*[äöÄÖ]+.*")){
 			System.out.println(sanat.get(3));
@@ -106,6 +157,12 @@ public class SalasananVahvuus {
 		}
 		return true;
 	}
+	/**
+	 * Jos salasana sisältää huutomerkin palauttaa funktio arvon false
+	 * @param salasana String tyyppinen parametri saadaan lueSalasana funktiosta
+	 * @param sanat Listasta löytyvät printlinet jotka kertovat mitä funktiossa vaaditaan käyttäjältä
+	 * @return true jos salasana läpäisee kriteerin
+	 */
 	private static boolean salasananHUUTO(String salasana, ArrayList<String> sanat) {
 		if (salasana.contains("!")){ 
 			System.out.println(sanat.get(5)); 
@@ -113,6 +170,12 @@ public class SalasananVahvuus {
 		} 
 		return true;
 	}
+	/**
+	 * Jos salasana ei sisällä numeroita palauttaa funktio arvon false
+	 * @param salasana String tyyppinen parametri saadaan lueSalasana funktiosta
+	 * @param sanat Listasta löytyvät printlinet jotka kertovat mitä funktiossa vaaditaan käyttäjältä
+	 * @return true jos salasana läpäisee kriteerin
+	 */
 	private static boolean salasananNumero(String salasana, ArrayList<String> sanat) {
 		if (!salasana.matches(".*\\d+.*")){  
 			System.out.println(sanat.get(2));  
@@ -120,6 +183,12 @@ public class SalasananVahvuus {
 		}  
 		return true;
 	}
+	/**
+	 * Jos salasana on alle 10 merkkiä pitkä palauttaa funktio arvon false
+	 * @param salasana String tyyppinen parametri saadaan lueSalasana funktiosta
+	 * @param sanat Listasta löytyvät printlinet jotka kertovat mitä funktiossa vaaditaan käyttäjältä
+	 * @return true jos salasana läpäisee kriteerin
+	 */
 	private static boolean salasananPituus(String salasana, ArrayList<String> sanat) {
 		if (salasana.length() < 10){
 			System.out.println(sanat.get(0));  
@@ -127,6 +196,12 @@ public class SalasananVahvuus {
 		}
 		return true;
 	}
+	/**
+	 * Jos salasana sisältää kiellettyjä merkkehä kuten @, û tai ; niin palauttaa funktio arvon false
+	 * @param salasana String tyyppinen parametri saadaan lueSalasana funktiosta
+	 * @param sanat Listasta löytyvät printlinet jotka kertovat mitä funktiossa vaaditaan käyttäjältä
+	 * @return true jos salasana läpäisee kriteerin
+	 */
 	public static boolean salasananTarkistaMerkit(String salasana, ArrayList<String> sanat){
 		ArrayList<String> väärätmerkit = new ArrayList<String>();
 		väärätmerkit.add("©");
@@ -140,9 +215,14 @@ public class SalasananVahvuus {
 		}
 		return true;
 	}
+	/**
+	 * Funktion avulla kysytään käyttäjältä salasana lukija scannerin avulla. Salasana tallennetaan kirjoittajan avulla tiedosto.txt nimiseen tiedostoon.
+	 * @return Palauttaa String tyyppisen muuttujan joka saadaan käyttäjän inputista
+	 * @throws FileNotFoundException Mikäli tiedostoa ei löydy.
+	 */
 	private static String lueSalasana() throws FileNotFoundException {  
 		PrintWriter kirjoittaja = new PrintWriter("Salasana.txt");
-		System.out.println("Anna salasana, sen tulee sisältää ainakin 10 merkkiä, isoja ja pieniä kirjaimia, numeroita sekä ääkkösiä. Ei saa olla välilyöntiä eikä huutomerkkiä, salasana ei myöskään saa olla yli 50 merkkiä pitkä");  
+		System.out.println("Anna salasana, sen tulee sisältää ainakin 10 merkkiä, sanan 'data',  isoja ja pieniä kirjaimia, numeroita sekä ääkkösiä. Ei saa olla välilyöntiä, merkkejä û, @ tai ; eikä huutomerkkiä, salasana ei myöskään saa olla yli 50 merkkiä pitkä");  
 		String salasana = lukija.nextLine();
 
 		kirjoittaja.print(salasana);
